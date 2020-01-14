1f378f47b7240801b30c5fd222e10c1870ea5a3b
MorrisYourFriend <Artbocharoff@yandex.ru>
2020-01-14 19:20:36 +0300

    -

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,13 +1,18 @@
+import java.util.*;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    public static int saveCounter = 0;
     Resume[] storage = new Resume[10000];
 
     void clear() {
     }
 
     void save(Resume r) {
+        storage[saveCounter] = r;
+        saveCounter++;
     }
 
     Resume get(String uuid) {
@@ -21,10 +26,18 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        /**
+        Resume[] list = new Resume[saveCounter];
+        for (int i = 0; i <= saveCounter; i++)
+        {
+            list[i] = storage[i];
+        }
+        return list;
+         */
+        return storage;
     }
 
     int size() {
-        return 0;
+        return saveCounter;
     }
 }
